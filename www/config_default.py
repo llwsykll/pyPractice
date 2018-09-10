8f3d4b11253f966bbdcd6b39cd517b04e7e837ca
llwsykll <574833620@qq.com>
2018-09-10 17:13:49 +0800

    0910.

---
+++ www/config_default.py
@@ -0,0 +1,14 @@
+'''
+Default configurations.
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'root',
+        'password':'password',
+        'db':'practice'
+    }
+}
